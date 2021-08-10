3748cb4465416734c2fd7d586d8bc96430097bb0
TWAllison <tylerallison55@gmail.com>
2021-08-10 02:43:08 -0600

    add variables and console log to .js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! js is Running!");
+function fight() {
+    window.alert("The Fight Has Begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+console.log(11+52);
+console.log("your robot's name  is " + playerName);
+//fight()
